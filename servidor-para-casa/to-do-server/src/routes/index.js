989beb554179271edeaeea20a3efd21f37e92adb
Kamila Silva <kamillasilva140@gmail.com>
2021-07-15 21:46:18 -0300

    entrega to-do-server

--- servidor-para-casa/to-do-server/src/routes/index.js
+++ servidor-para-casa/to-do-server/src/routes/index.js
@@ -5,7 +5,7 @@ router.get("/", (request, response)=>{
     response.status(200).json({
         "titulo": "To-do Api - Reprograma",
         "version": "1.0.0",
-        "mensagem": "bem vinda bebe"
+        "mensagem": "bem vinde bebe"
     })
 })
 module.exports = router
