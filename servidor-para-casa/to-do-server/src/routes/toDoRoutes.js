5d08df7ccf90382ca9c2bced2951b06f1c44e92a
Kamila Silva <kamillasilva140@gmail.com>
2021-07-15 21:47:00 -0300

    entrega to-do-server

--- servidor-para-casa/to-do-server/src/routes/toDoRoutes.js
+++ servidor-para-casa/to-do-server/src/routes/toDoRoutes.js
@@ -9,4 +9,10 @@ router.post("/cadastrar", controller.createTask)
 
 router.delete("/:id", controller.deleteTask)
 
+router.put("/:id", controller.replaceTask)
+
+router.patch("/:id", controller.updateTitle)
+
+//router.patch("/:id", controller.updateAnyThing)
+
 module.exports = router
